d3c965b2da2fbdcbad1d82049878e4e7c9aaa0c3
RonaldWHITE <ronald@white.nl>
2016-09-13 16:51:12 +0200

    initial commit

---
+++ src/config.js
@@ -0,0 +1,23 @@
+import angular from 'angular';
+import uiRouter from 'angular-ui-router';
+
+const app = angular.module('app', [uiRouter]);
+
+app.config(($stateProvider, $urlRouterProvider, $locationProvider) => {
+	$urlRouterProvider.otherwhise('/');
+	$stateProvider
+		.state('todos', {
+			url: '/',
+			template: require('todos/todos.html')
+		})
+		.state('about', {
+			url: '/about',
+			template: require('about/about.html')
+
+		})
+
+	$locationProvider.html5Mode(true);
+
+});
+
+export default app;
